b166d5c43245f8045539a3d8a6b416161e59d218
honeo <hone.mk2@gmail.com>
2015-12-23 15:34:08 +0900

    first

---
+++ index.js
@@ -0,0 +1,113 @@
+// Var
+const
+	_name = 'dotjs-cache',
+	doc = document,
+	head = doc.head || (doc.head = doc.getElementsByTagName('head')[0]),
+	lS  = localStorage,
+	cacheObj = JSON.parse(lS[_name] || '{}'),
+	Datenow = Date.now(),
+	cache_default = true,
+	cors_default = false,
+	exec_default = true,
+	expire_default = {date: 1},
+	retry_default = true;
+
+// 期限チェック
+for(let key in cacheObj){
+	if( cacheObj[key].expire < Datenow ){
+		delete cacheObj[key];
+	}
+}
+
+/*
+	モジュールの返り値になるやつ
+		引数が不正ならreject()
+		ログがあれば叩いてresolve()
+		ログがなければXHRで取得して実行
+			成功すればキャッシュ
+			失敗すれば通常読み込み
+*/
+function main({
+	url, cache=cache_default, cors=cors_default, exec=exec_default, expire=expire_default, retry=retry_default,
+}){
+	const target = cacheObj[url];
+	return new Promise( (resolve, reject)=>{
+		if(typeof url!=='string'){
+			reject(new TypeError('Invalid argument {url}'));
+		}else if( target ){
+			resolve(target.code);
+		}else{
+			const xhr = new XMLHttpRequest();
+			xhr.open('GET', (
+				cors===true ?
+					`http://cors.io/?u=${url}`:
+					url
+			));
+			xhr.onreadystatechange = ()=>{
+				if(xhr.readyState===4 && xhr.status===200){
+					resolve(xhr.responseText);
+				}
+			}
+			xhr.onerror = (e)=>{
+				reject(e);
+			}
+			xhr.send(null);
+		}
+	}).then( (code)=>{
+		try{
+			exec && new Function(`(function(){${code}}());`)();
+			// ログがなかった場合は保存
+			if( cache && !target ){
+				cacheObj[url] = {
+					expire: changeDate(expire).getTime(), // 期限日時 => ms
+					code
+				}
+				lS[_name] = JSON.stringify(cacheObj);
+			}
+		}catch(e){
+			return Promise.reject(e);
+		}
+	}).catch( (e)=>{
+		// 通常読み込み
+		retry && loadScript(url);
+		Promise.reject(e);
+	})
+}
+
+// 普通にscript要素を突っ込んで.js読み込み
+function loadScript(url){
+	const script = doc.createElement('script');
+	script.defer = true;
+	script.async = true;
+	script.src = url;
+	head.appendChild(script);
+}
+
+// dateインスタンスの日付変更
+function changeDate(obj){
+	if(typeof obj!=='object'){
+		throw new TypeError('argument is only {...}');
+	}
+	const time = obj.instance ?
+		new Date(obj.instance.getTime()):
+		new Date();
+	const year = obj.years || obj.year || obj.yrs || obj.yr || obj.y;
+	const mon = obj.months || obj.month || obj.mon || obj.mo;
+	const week = obj.weeks || obj.week || obj.wks || obj.wk || obj.w;
+	const date = obj.dates || obj.date || obj.days || obj.day || obj.d;
+	const hour = obj.hours || obj.hour || obj.hr || obj.h;
+	const min = obj.minutes || obj.minute || obj.min || obj.m;
+	const sec = obj.seconds || obj.second || obj.sec || obj.s;
+	const ms = obj.milliseconds || obj.millisecond || obj.msec || obj.ms;
+	typeof year==='number' && time.setFullYear(time.getFullYear() + year);
+	typeof mon==='number' && time.setMonth(time.getMonth() + mon);
+	typeof week==='number' && time.setDate(time.getDate() + (week*7));
+	typeof date==='number' && time.setDate(time.getDate() + date);
+	typeof hour==='number' && time.setHours(time.getHours() + hour);
+	typeof min==='number' && time.setMinutes(time.getMinutes() + min);
+	typeof sec==='number' && time.setSeconds(time.getSeconds() + sec);
+	typeof ms==='number' && time.setMilliseconds(time.getMilliseconds() + ms);
+	return time;
+}
+
+module.exports = main;
---
+++ readme.md
@@ -0,0 +1,29 @@
+# dotjs-cache
+[honeo/dotjs-cache](https://github.com/honeo/dotjs-cache)
+[dotjs-cache](https://www.npmjs.com/package/dotjs-cache)
+
+## なにこれ
+.jsファイルをXHRで取得して実行する。  
+成功すればlocalStorageにキャッシュして次からはそれを使う。
+
+## 使い方
+Promiseインスタンスを返す。
+```js
+const dotjsCache = require('dotjs-cache');
+
+dotjsCache({
+	url: 'http://example.com/hoge.js'
+}).then( (arg)=>{
+	console.log('hoge');
+});
+```
+設定は引数で渡すオブジェクトのプロパティから行う。  
+
+|properties	| 初期設定, 説明	|
+|:---------:|:---------------|
+|	cache	| true, キャッシュ使用の有無        				|
+|	cors	| false, [cors.io](http://cors.io/)使用の有無 |
+|	exec	| true, .jsファイル実行の有無						 |
+|	expire	| {date: 1}, キャッシュ期限の設定
+|	retry	| true, 失敗時の通常読み込み(script要素.src)の有無|
+|	url		| __必須__, 対象にする.jsファイルのアドレス |
